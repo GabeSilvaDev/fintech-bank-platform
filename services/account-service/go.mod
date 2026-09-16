7166c0d45e6d188b36aa0443dd02b9b5c5d8355b
GabeSilvaDev <spanxscomercial@gmail.com>
2026-09-16 14:26:27 -0300

    🎉 feat(account-service): scaffold service with config and tooling

---
+++ services/account-service/go.mod
@@ -0,0 +1,17 @@
+module github.com/fintech-bank-platform/account-service
+
+go 1.25
+
+require (
+	github.com/fintech-bank-platform/pkg v0.0.0
+	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace github.com/fintech-bank-platform/pkg => ../../pkg
---
+++ services/account-service/internal/config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"time"
+
+	"github.com/fintech-bank-platform/account-service/internal/contracts"
+	"github.com/fintech-bank-platform/pkg/env"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server    contracts.ServerConfig
+	Kafka     contracts.KafkaConfig
+	Cassandra contracts.CassandraConfig
+	Consumer  contracts.ConsumerConfig
+	Log       contracts.LogConfig
+}
+
+func New() (*Config, error) {
+	_ = godotenv.Load()
+
+	return &Config{
+		Server: contracts.ServerConfig{
+			Host:            env.Get("SERVER_HOST", "0.0.0.0"),
+			Port:            env.Get("SERVER_PORT", "8082"),
+			ReadTimeout:     env.GetDuration("SERVER_READ_TIMEOUT", 30*time.Second),
+			WriteTimeout:    env.GetDuration("SERVER_WRITE_TIMEOUT", 30*time.Second),
+			IdleTimeout:     env.GetDuration("SERVER_IDLE_TIMEOUT", 120*time.Second),
+			ShutdownTimeout: env.GetDuration("SERVER_SHUTDOWN_TIMEOUT", 10*time.Second),
+		},
+		Kafka: contracts.KafkaConfig{
+			Brokers:        env.SplitAndTrim(env.Get("KAFKA_BROKERS", "localhost:9092")),
+			GroupID:        env.Get("KAFKA_GROUP_ID", "account-service"),
+			WriteTimeout:   env.GetDuration("KAFKA_WRITE_TIMEOUT", 5*time.Second),
+			BatchTimeout:   env.GetDuration("KAFKA_BATCH_TIMEOUT", 10*time.Millisecond),
+			PublishTimeout: env.GetDuration("KAFKA_PUBLISH_TIMEOUT", 20*time.Second),
+			MaxAttempts:    env.GetIntMin("KAFKA_MAX_ATTEMPTS", 3, 1),
+		},
+		Cassandra: contracts.CassandraConfig{
+			Hosts:          env.SplitAndTrim(env.Get("CASSANDRA_HOSTS", "localhost:9042")),
+			Keyspace:       env.Get("CASSANDRA_KEYSPACE", "fintech_accounts"),
+			Consistency:    env.Get("CASSANDRA_CONSISTENCY", "LOCAL_QUORUM"),
+			Timeout:        env.GetDuration("CASSANDRA_TIMEOUT", 10*time.Second),
+			ConnectTimeout: env.GetDuration("CASSANDRA_CONNECT_TIMEOUT", 10*time.Second),
+			MigrationsPath: env.Get("CASSANDRA_MIGRATIONS_PATH", "migrations"),
+		},
+		Consumer: contracts.ConsumerConfig{
+			RetryBackoff: env.GetDurations("CONSUMER_RETRY_BACKOFF", []time.Duration{200 * time.Millisecond, time.Second, 5 * time.Second}),
+		},
+		Log: contracts.LogConfig{
+			Level:  env.Get("LOG_LEVEL", "info"),
+			Pretty: env.GetBool("LOG_PRETTY", false),
+		},
+	}, nil
+}
